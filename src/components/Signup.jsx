e9fc016339aa9ecf6d3e1e66659b27da613b2db6
palak porwal <99019333+39palakved@users.noreply.github.com>
2025-04-27 15:20:34 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://vertx-backend.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
